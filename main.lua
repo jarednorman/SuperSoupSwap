c04bb0fccb52c07b456d4a48233589612079c8fc
Jared Norman <moc.ae-axt@txa-ea.com>
2011-09-14 20:17:03 -0700

    gogogogogogo

---
+++ main.lua
@@ -0,0 +1,30 @@
+
+require 'middleclass'
+
+function love.load()
+end
+
+function love.update(dt)
+end
+
+function love.draw()
+end
+
+function love.mousepressed(x, y, button)
+end
+
+function love.mousereleased(x, y, button)
+end
+
+function love.keypressed(key, unicode)
+end
+
+function love.keyreleased(key, unicode)
+end
+
+function love.focus(f)
+end
+
+function love.quit()
+end
+
