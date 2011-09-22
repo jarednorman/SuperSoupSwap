c7f1cd2fefc26974e2affc730b3c939b315cefbd
Jared Norman <moc.ae-axt@txa-ea.com>
2011-09-22 16:58:38 -0700

    Random board population.

--- game.lua
+++ game.lua
@@ -1,28 +1,90 @@
 module(..., package.seeall)
 
+Block = class("Block")
+
+function Block:initialize(colour, half)
+	loadImage('blue.png')
+	loadImage('peaches.png')
+	loadImage('mushroom.png')
+	loadImage('tomato.png')
+	loadImage('bluehalf.png')
+	loadImage('peacheshalf.png')
+	loadImage('mushroomhalf.png')
+	loadImage('tomatohalf.png')
+	self.colour = colour
+	self.half = half
+end
+
+function Block:draw(x,y)
+	local sprite = self.colour .. self.half .. ".png"
+	love.graphics.draw(graphics[sprite], x, y, 0, 2, 2)
+end
+
 Game = class("Game")
 
 function Game:initialize()
-	print("SDFN")
 end
 
 function Game:reinitialize()
-	print("Game reinited")
+	math.randomseed(os.time())
+	self.columns = {}
+	for n=1,8 do
+		self.columns[n] = {}
+	end
+	-- POPULATE --
+	local initialBlockCount = 50
+	local colours = {'tomato', 'peaches', 'blue', 'mushroom'}
+	local haff = {'','half'}
+	local n = 0
+	while n < initialBlockCount do
+		local column = math.random(1,#self.columns)
+		local colour = colours[math.random(1,#colours)]
+		local half = haff[math.random(1,2)]
+		if self:insertBlock(column, Block(colour, half)) then
+			n = n + 1
+		end
+	end
+end
+
+function Game:insertBlock(column, block)
+	-- if the block won't fit, return false
+	local c = self.columns[column]
+	local columnHeight = 0
+	for k,v in ipairs(c) do
+		if v.half == 'half' then
+			columnHeight = columnHeight + 1
+		else
+			columnHeight = columnHeight + 2
+		end
+	end
+	-- add the block
+	local blockHeight = 1
+	if block.half == 'half' then blockHeight = 2 end
+	if columnHeight + blockHeight > 17 then
+		return false
+	end
+	table.insert(c, block)
+	return true
 end
 
 function Game:locateSquare(x, y)
-	local x = love.graphics.getWidth()*.85 - graphics['bg.png']:getWidth()*4 + x*44
-	local y = love.graphics.getHeight() - graphics['bg.png']:getHeight()*2*3 + y*30
+	local x = x - 1
+	local y = 18 - y
+	x = love.graphics.getWidth()*.85 - graphics['bg.png']:getWidth()*4 + x*44
+	y = love.graphics.getHeight() - graphics['bg.png']:getHeight()*2*3 + y*30
 	return x, y
 end
 
-function Game:convertMousePosition(x, y)
+function Game:convertScreenPosition(x, y)
 	local x = (x - love.graphics.getWidth()*.85 + graphics['bg.png']:getWidth()*4)/44
 	local y = (y - love.graphics.getHeight() + graphics['bg.png']:getHeight()*2*3)/30
+	x = x + 1
+	y = 18 + y
 	return x, y
 end
 
 function Game:draw()
+	-- Draw Board --
 	love.graphics.draw(graphics['bg.png'],
 							love.graphics.getWidth()*.85 - graphics['bg.png']:getWidth()*4,
 							love.graphics.getHeight() - graphics['bg.png']:getHeight()*2,
@@ -47,7 +109,21 @@ function Game:draw()
 							love.graphics.getWidth()*.85 - graphics['bg.png']:getWidth()*2,
 							love.graphics.getHeight() - graphics['bg.png']:getHeight()*2*3,
 							0, 2, 2)
+	-- Draw blocks --
+	for k,v in ipairs(self.columns) do
+		local verticalBlock = 0
+		for n,b in ipairs(v) do
+			if b.half == 'half' then
+				verticalBlock = verticalBlock + 1
+			else
+				verticalBlock = verticalBlock + 2
+			end
+			local x,y = self:locateSquare(k, verticalBlock)
+			b:draw(x,y)
+		end
+	end
 
+	-- Draw HUD --
 	love.graphics.draw(graphics['SUPER.png'],
 							6,
 							6,
@@ -60,4 +136,6 @@ function Game:draw()
 							6,
 							14 + graphics['SUPER.png']:getHeight()*2 + graphics['SOUP.png']:getHeight()*2,
 							0, 2, 2)
+
+
 end
