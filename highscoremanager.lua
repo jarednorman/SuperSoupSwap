2cafa582ea57ab85e665ae329170fd5d37a02c30
Alistair Norman <4l15741r@gmail.com>
2011-10-19 12:41:32 -0700

    skeletoned highscoremanager.lua

---
+++ highscoremanager.lua
@@ -0,0 +1,21 @@
+module(..., package.seeall)
+
+HighscoreManager = class("HighscoreManager")
+
+function HighscoreManager:initialize()
+	--Load highscores from file
+	--Create file if it does no exist
+end
+
+function HighscoreManager:getAverageScore()
+end
+
+function HighscoreManager:getMediaScore()
+end
+
+function HighscoreManager:getHighscores(n)
+end
+
+function HighscoreManager:writeOut()
+end
+
